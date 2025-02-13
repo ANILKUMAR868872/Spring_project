007e82f8135e3164064026b1a35af4ba5f0ace23
ANILKUMAR868872 <143528972+ANILKUMAR868872@users.noreply.github.com>
2025-02-13 13:47:28 +0530

    Spring_API

---
+++ Kumar/src/main/java/com/example/Kumar/KumarApplication.java
@@ -0,0 +1,14 @@
+package com.example.Kumar;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumarApplication.class, args);
+	}
+
+}
